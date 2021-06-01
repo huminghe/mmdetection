1d7ca05ae403fef656be93ec96002abe50518bba
huminghe <huminghe@126.com>
2021-06-01 16:49:21 +0800

    refactor(detection): refine railway object detection

--- test.py
+++ test.py
@@ -2,16 +2,15 @@ from mmdet.apis import init_detector, inference_detector
 import mmcv
 import os
 from tqdm import tqdm
-import sys
 
-config_file = sys.argv[1]
-checkpoint_file = sys.argv[2]
-input_path = sys.argv[3]
-output_path = sys.argv[4]
-device = int(sys.argv[5])
+# Specify the path to model config and checkpoint file
+config_file = '/data/huminghe/mmdetection/configs/railway/gfl_0508_all_merged.py'
+checkpoint_file = '/data/huminghe/mmdetection/model_0508_gfl/epoch_24.pth'
+input_path = '/data/huminghe/test/detection_test'
+output_path = '/data/huminghe/tmp/food_gfl'
 
 # build the model from a config file and a checkpoint file
-model = init_detector(config_file, checkpoint_file, device='cuda:{}'.format(device))
+model = init_detector(config_file, checkpoint_file, device='cuda:1')
 
 listdir = os.listdir(input_path)
 
@@ -21,4 +20,4 @@ for file_name in tqdm(listdir):
         result = inference_detector(model, img_path)
         out_file_path = os.path.join(output_path, file_name)
 
-        model.show_result(img_path, result, out_file=out_file_path)
+        model.show_result(img_path, result, out_file=out_file_path, visual_label_offset=5)
